a73ae8e65bc6275dd5fec6dcb2cc8bdbe5a2efe2
caozheng <435932323@qq.com>
2019-05-28 18:34:35 +0800

    修改

--- formats.py
+++ formats.py
@@ -14,14 +14,16 @@ class Formats(object):
         header = '车次 车站 时间 历时 一等 二等 软卧 硬卧 硬座 无座'.split()
         ptable._set_field_names(header)
         for train in self.datas:
-            trains = train.get('queryLeftNewDTO')
-            if trains is not None and trains != '':
-                initial = trains['station_train_code'].lower()[0]
-                if not self.options or initial in self.options:
-                    ptable.add_row([trains['station_train_code'], Fore.GREEN 
-                        + "%s -> %s" % (trains['from_station_name'], 
-                        trains['to_station_name']) + Fore.RESET, 
-                        Fore.RED + "%s -> %s" % (trains['start_time'], trains['arrive_time']) 
-                        + Fore.RESET, trains['lishi'], trains['zy_num'], trains['ze_num'], 
-                        trains['rw_num'], trains['yw_num'], trains['yz_num'], trains['wz_num']])
+            ptable.add_row(['1','2','3','4','5','6','7','8','9','10'])
+            # trains = train.get('queryLeftNewDTO')
+            # if trains is not None and trains != '':
+            #     initial = trains['station_train_code'].lower()[0]
+            #     if not self.options or initial in self.options:
+            #         ptable.add_row([trains['station_train_code'], Fore.GREEN 
+            #             + "%s -> %s" % (trains['from_station_name'], 
+            #             trains['to_station_name']) + Fore.RESET, 
+            #             Fore.RED + "%s -> %s" % (trains['start_time'], trains['arrive_time']) 
+            #             + Fore.RESET, trains['lishi'], trains['zy_num'], trains['ze_num'], 
+            #             trains['rw_num'], trains['yw_num'], trains['yz_num'], trains['wz_num']])
+        print(ptable)
         return ptable
